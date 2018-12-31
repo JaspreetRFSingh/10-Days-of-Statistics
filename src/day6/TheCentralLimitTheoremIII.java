c33b23e25d19a9c3ab8b676870ebf6d2c9c85541
Jaspreet Singh <JaspreetRFSingh@users.noreply.github.com>
2018-12-31 16:18:51 +0530

    Day-6

---
+++ src/day6/TheCentralLimitTheoremIII.java
@@ -0,0 +1,15 @@
+package day6;
+
+public class TheCentralLimitTheoremIII {
+
+	  public static void main(String[] args) {
+	        double mean = 500;
+	        double std = 80;
+	        int n = 100;
+	        double zScore = 1.96;
+	        double marginOfError = zScore * std / Math.sqrt(n);
+
+	        System.out.printf("%.2f%n", mean - marginOfError);
+	        System.out.printf("%.2f%n", mean + marginOfError);
+	    }
+}
